4f165886ee330196da57f5bccdefaa30c23c05b7
CodeLaunch <livesiddharth@hotmail.com>
2015-12-20 09:47:15 +0530

    CaterInfo

---
+++ src/main/java/com/cater/tos/beans/CaterInfo.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.cater.tos.beans;
+
+/**
+ * @author siddharth
+ *
+ */
+public class CaterInfo {
+	private Integer id;
+	private String name;
+	private String address;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
+}
